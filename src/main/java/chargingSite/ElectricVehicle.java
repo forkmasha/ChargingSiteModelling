b4c3e66b1b1ab52755881de3d14dc96132d54cc2
franzl <gerald.franzl@donau-uni.ac.at>
2023-10-06 16:15:43 +0200

    Negative SoC and charged energy patched, BETA distribution returns demand values above 1 for mean > 0.7, which should not happen! To be done - for now demand is patched into [0..1] at the cost of accurate distribution.

--- src/main/java/chargingSite/ElectricVehicle.java
+++ src/main/java/chargingSite/ElectricVehicle.java
@@ -33,7 +33,11 @@ public class ElectricVehicle {
         this.chargingPower = maxPower;
         this.batteryCapacity = batteryCapacity;
         this.demandDistribution = Distribution.create(demandDistributionType);
-        this.chargeDemand = batteryCapacity * demandDistribution.getSample(0.8);  // TO BO DONE ???  via GUI (0.1, 0.2, ... 0.9)
+        double demand = demandDistribution.getSample(0.70);  // TO BO DONE ???  via GUI (0.1, 0.2, ... 0.9)
+        //bad patch !!! TO BE DONE better, i.e., BETA distribution sometimes returns values slightly above 1 for mean > 0.7 ...
+        if(demand < 0) {System.out.println("Warning: negative demand  " + demand + " was converted to positive."); demand *= -1; }
+        if(demand > 1) {System.out.println("Warning: demand " + demand + " > 1 was converted to 1/demand."); demand = 1/demand; }
+        this.chargeDemand = demand * batteryCapacity;
         this.stateOfCharge = 1 - chargeDemand/batteryCapacity;
         this.energyCharged = 0;
     }
@@ -55,44 +59,74 @@ public class ElectricVehicle {
     }
 
     public void addEnergyCharged(double duration, double sitePower) {
-        if (stateOfCharge>=1) { return; }
-        double chargedEnergy = duration * chargingPower;
+        if (this.stateOfCharge>=1) { return; }
         this.updateChargingPower(sitePower);
-        if (stateOfCharge + chargedEnergy/batteryCapacity > 1) {
-            chargedEnergy = (1-stateOfCharge) * batteryCapacity;
+        double chargedEnergy = duration * this.chargingPower;
+        if(chargedEnergy<0) {System.out.println("ERROR: Charged Energy is negative!"); System.exit(1);}
+        //if(chargingPower<0) {System.out.println("ERROR: Negative charging power!"); System.exit(1);}
+        if (this.stateOfCharge + chargedEnergy/this.batteryCapacity > 1) {
+            chargedEnergy = (1-this.stateOfCharge) * this.batteryCapacity;
         }
-        this.stateOfCharge += chargedEnergy/batteryCapacity;
+        this.stateOfCharge += chargedEnergy/this.batteryCapacity;
         this.energyCharged += chargedEnergy;
-        //System.out.println("duration = " + duration);
-        //System.out.println("chargingPower = " + chargingPower);
-        //System.out.println("chargedEnergy = " + chargedEnergy);
+
+        // tracking down negative SoC...
+        if(this.stateOfCharge<0) {
+            System.out.println("duration = " + duration);
+            System.out.println("chargingPower = " + chargingPower);
+            System.out.println("chargedEnergy = " + chargedEnergy);
+            System.out.println("SoC = " + stateOfCharge);
+            System.out.println("ERROR: Negative SoC!"); System.exit(1);
+        }
+        /* if(this.energyCharged<0) {
+            System.out.println("duration = " + duration);
+            System.out.println("chargingPower = " + chargingPower);
+            System.out.println("chargedEnergy = " + chargedEnergy);
+            System.out.println("ERROR: EnergyCharged became negative !");
+            System.exit(1);
+        } */
     }
 
     public void resetEnergyCharged() {this.energyCharged = 0.0; }
     
     public void updateChargingPower(double sitePower) {
-        double newChargingPower = this.chargingPower;
+        // double newChargingPower = this.chargingPower;
+        if(this.chargingPower<0) {System.out.println("ERROR: Negative charging power prior update!");}
+        if(this.stateOfCharge<0) {System.out.println("ERROR: Negative SoC prior charging power update!");}
         double maxChargingPointPower = this.getChargingPoint().getMaxPower();
         double maxChargingSitePower = this.getSiteModel().getChargingSite().getMaxSitePower();
         if (this.stateOfCharge>=1) {
             this.chargingPower = 0;
         } else if (this.stateOfCharge>0.8) {  // adjust charging power to current state of charge
             this.chargingPower = this.batteryCapacity * (1 - stateOfCharge ) / 0.2 ;
+            if(this.chargingPower<0) {System.out.println("ERROR: Negative charging power for SoC >0.8!");}
         } else if (this.stateOfCharge>0.2) {  // adjust charging power to current state of charge
             this.chargingPower = this.batteryCapacity * 10 * (1.1 - stateOfCharge);
+            if(this.chargingPower<0) {System.out.println("ERROR: Negative charging power for SoC in 0.2 .. 0.8!");}
         } else { // adjust charging power to current state of charge
-            this.chargingPower = this.batteryCapacity + 2 * this.batteryCapacity * stateOfCharge / 0.2;
+            this.chargingPower = this.batteryCapacity + 2 * this.batteryCapacity * this.stateOfCharge / 0.2;
+            if(this.chargingPower<0) {System.out.println("ERROR: Negative charging power for SoC <0.2!");}
         }
+
+        //System.out.println("batteryCapacity = " + this.batteryCapacity);
+        //System.out.println("stateOfCharge = " + this.stateOfCharge);
+        //System.out.println("chargingPower = " + this.chargingPower);
 
         if (this.chargingPower>maxChargingPointPower) {  // limit charging power to max possible
             this.chargingPower = maxChargingPointPower;
+            if(this.chargingPower<0) {System.out.println("ERROR: Negative charging power after PointLimiting!");}
         }
 
-        if (this.chargingPower>maxChargingSitePower) {  // limit charging power to max possible
+        if (sitePower>maxChargingSitePower) {  // limit charging power to max possible
             this.chargingPower *= maxChargingSitePower/sitePower;
+            if(this.chargingPower<0) {System.out.println("ERROR: Negative charging power after SiteLimiting!");}
         }
-
-        System.out.println(this.id + ": SoC = " + this.stateOfCharge + " chargingPower = " + this.chargingPower);
+        // looking for negative charging power (error)
+        if(this.chargingPower<0 || this.stateOfCharge<0) {
+            System.out.println("ERROR: " + this.id + ": SoC = " + this.stateOfCharge + " chargingPower = " + this.chargingPower);
+            System.out.println("Battery Capacity: " + this.batteryCapacity + " Energy charged:" + this.getEnergyCharged());
+            System.exit(1);
+        }
     }
 
     public void setPlugType(String plugType) {

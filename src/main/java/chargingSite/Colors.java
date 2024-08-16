674adad4df45c41bc5071246acd5cf9cec06ff18
Maria Forkaliuk <forkmasha100@gmail.com>
2024-08-16 21:59:24 +0300

    Forgot

---
+++ src/main/java/chargingSite/Colors.java
@@ -0,0 +1,22 @@
+package chargingSite;
+
+import java.awt.Color;
+
+public class Colors{
+    public static final Color LIGHT_PINK = new Color(255, 182, 193);
+    public static final Color LAVENDER = new Color(230, 230, 250);
+    public static final Color PEACH = new Color(255, 218, 185);
+    public static final Color BLUE = new Color(173, 216, 230);
+    public static final Color LIGHT_BLUE = new Color(200, 200, 240);
+    public static final Color DARK_BLUE = new Color(136, 186, 242);
+    public static final Color ORANGE = new Color(255, 175, 128);
+    public static final Color RED = new Color(255, 102, 102);
+    public static final Color LIGHT_GREEN = new Color(200, 240, 200);
+    public static final Color LIGHT_BLUE1 = new Color(180, 200, 240);
+    public static final Color LIGHT_YELLOW = new Color(240, 210, 160);
+    public static final Color LIGHT_PINK1 = new Color(240, 200, 220);
+    public static final Color LIGHT_YELLOW1 = new Color(240, 240, 200);
+    public static final Color LIGHT_KREM = new Color(240, 230, 210);
+    public static final Color LAVANDA = new Color(220, 200, 240);
+
+}

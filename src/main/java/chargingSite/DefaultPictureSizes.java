152acdabcb25a09fe1a3c97a1fa04c64954e2f5f
Maria Forkaliuk <forkmasha100@gmail.com>
2025-01-24 10:41:51 -1000

    Update+1 file

---
+++ src/main/java/chargingSite/DefaultPictureSizes.java
@@ -0,0 +1,8 @@
+package chargingSite;
+
+public class DefaultPictureSizes {
+    public static final int PNG_WIDTH = 2400;
+    public static final int PNG_HEIGTH = 1560;
+    public static final int SVG_WIDTH = 1200;
+    public static final int SVG_HEIGTH = 730;
+}
